78464d2b8344b7049f91e71edabd0e4854cc9be9
bo.zhang01 <bo.zhang01@ucarinc.com>
2016-12-07 14:51:07 +0800

    文件提交

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '10.104.20.123',
+        'port': 3306,
+        'user': 'developer',
+        'password': 'developer1015',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
